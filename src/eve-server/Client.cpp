f66fbae6a4a213a67ff95ef66da84e4ec50b8f83
celinor1982 <larry.antolin@gmail.com>
2025-05-10 18:23:01 -0500

    Update Client.cpp
    Added iRef pointer for delete

--- src/eve-server/Client.cpp
+++ src/eve-server/Client.cpp
@@ -1776,6 +1776,7 @@ void Client::RemoveMissionItem(uint16 typeID, uint32 qty)
             } else {
                 count -= iRef->quantity();
                 iRef->Delete();
+                iRef = nullptr;
             }
         }
     }
@@ -1789,6 +1790,7 @@ void Client::RemoveMissionItem(uint16 typeID, uint32 qty)
             } else {
                 count -= iRef->quantity();
                 iRef->Delete();
+                iRef = nullptr;
             }
         }
     }
