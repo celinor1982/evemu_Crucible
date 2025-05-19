14ed2c28141eb42a3ed68045d304b3d940658adf
celinor1982 <konnal1982@gmail.com>
2025-05-18 21:44:27 -0500

    Updated syntax for orderRange integer

--- src/eve-common/EVE_Market.h
+++ src/eve-common/EVE_Market.h
@@ -96,7 +96,7 @@ namespace Market {
         bool isCorp :1;
         bool contraband :1;
         uint8 jumps;
-        int16 orderRange;
+        uint32 orderRange;
         uint16 typeID;
         uint16 accountKey;
         uint32 orderID;
